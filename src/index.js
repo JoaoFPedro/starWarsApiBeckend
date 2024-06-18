cc956eef290c74a6a0e3464d9a26bdeba41c199d
João Pedro <joao.pedro@dtidigital.com.br>
2024-06-18 18:07:29 -0300

    create update and delete methods

--- src/index.js
+++ src/index.js
@@ -14,11 +14,13 @@ const Movie = mongoose.model("Movie", {
   trailer_url: String,
 });
 
+//List Method
 app.get("/", async (req, res) => {
   const filmes = await Movie.find();
   res.send(filmes);
 });
 
+// Create Method
 app.post("/", async (req, res) => {
   const movies = new Movie({
     title: req.body.title,
@@ -28,6 +30,21 @@ app.post("/", async (req, res) => {
   });
   await movies.save();
   res.send(movies);
+});
+
+//Delete Method
+app.delete("/:id", async (req, res) => {
+  const film = await Movie.findByIdAndDelete(req.params.id);
+  return res.send(film);
+});
+
+//Update Method
+app.put("/:id", async (req, res) => {
+  const film = await Movie.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    //Posso passar todos os campos que desejo que sejam editados
+  });
+  return res.send(film);
 });
 
 app.listen(port, () => {
