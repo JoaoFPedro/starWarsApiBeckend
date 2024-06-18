ae183ff84cb04568509271fd759e4abe3ca94f9c
João Pedro <joao.pedro@dtidigital.com.br>
2024-06-18 16:49:30 -0300

    creating api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-const express = require('express');
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const moongose = require("mongoose");
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+mongoose.connect(
+  "mongodb+srv://joaofpsilva98:N2XdX6cbGVqbjczc@starwarsapi.x0iwbpo.mongodb.net/?retryWrites=true&w=majority&appName=StarWarsAPI"
+);
+
+const Movie = mongoose.model("Movie", {
+  title: String,
+  description: String,
+  image_url: String,
+  trailer_url: String,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/", async (req, res) => {
+  const movies = new Movie({
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url,
+  });
+  await movies.save();
+  res.send(movies);
+});
 
 app.listen(port, () => {
-    console.log(`App running on port: ${port}` )
-})
+  console.log(`App running on port: ${port}`);
+});
